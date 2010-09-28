d2bc17d707819416a1f35570f4b16e57b77e2d9a
supermatthew <supermatthew@b11d8cf0-f614-4026-9bf6-8f2a8c9f8c2d>
2010-09-28 08:04:23 +0000

    Woot more work.

--- trunk/Server/Module/Login.cs
+++ trunk/Server/Module/Login.cs
@@ -38,7 +38,7 @@ using Tortoise.Shared.Net;
 
 namespace Tortoise.Server.Module
 {
-	protected class LoginLoader : ModuleLoader
+	class LoginLoader : ModuleLoader
 	{
 		public const ushort ClientModuleComID = 10010;
 		public const ushort ServerModuleComID = 20010;
--- trunk/SharedCode/IO/ByteReader.cs
+++ trunk/SharedCode/IO/ByteReader.cs
@@ -37,131 +37,142 @@ using System.Text;
 
 namespace Tortoise.Shared.IO
 {
-	/// <summary>
-	/// Reads from a byte array.
-	/// </summary>
-	public class ByteReader
-	{
-		private byte[] _array;
-		private int _pos;
-		private ByteConverter _bc;
-		private Encoding _encoder ;
-		
-		
-		public ByteReader(byte[] array)
-		{
-			_array = array;
-			init();
-			
-		}
-		public ByteReader(Stream stream, int offset, int length)
-		{
-			stream.Read(_array, offset, length);
-			init();
-		}
-		
-		public ByteReader(BinaryReader stream, int length)
-		{
-			_array = stream.ReadBytes(length);
-			init();
-		}
-		
-		private void init()
-		{
-			_encoder = Encoding.Unicode;
-			_pos = 0;
-			_bc = new ByteConverter();
-		}
-		
-		private void EnforceLength(int length)
-		{
-			if(_pos + length >= _array.Length) throw new IndexOutOfRangeException("End of the array!");
-		}
-		
-		public byte ReadByte()
-		{
-			EnforceLength(sizeof(byte));
-			//post addition returns the value then incriments it.
-			return _array[_pos++];
-		}
-		
-		public byte[] ReadBytes(int length)
-		{
-			EnforceLength(sizeof(byte) * length);
-			byte[] returnValue = new byte[length];
-			Array.Copy(_array, _pos, returnValue, 0, length);
-			_pos++;
-			return returnValue;
-		}
-		
-		public sbyte ReadSByte()
-		{
-			EnforceLength(sizeof(sbyte));
-			//post addition returns the value then incriments it.
-			return Convert.ToSByte(_array[_pos++]);
-		}
-		
-		public short ReadShort()
-		{
-			EnforceLength(sizeof(ushort));
-			//post addition returns the value then incriments it.
-			return BitConverter.ToInt16(_array, _pos++);
-		}
-		
-		public ushort ReadUShort()
-		{
-			EnforceLength(sizeof(ushort));
-			//post addition returns the value then incriments it.
-			return BitConverter.ToUInt16(_array, _pos++);
-		}
-		
-		public int ReadInt()
-		{
-			EnforceLength(sizeof(int));
-			//post addition returns the value then incriments it.
-			return BitConverter.ToInt32(_array, _pos++);
-		}
-		
-		public uint ReadUInt()
-		{
-			EnforceLength(sizeof(uint));
-			//post addition returns the value then incriments it.
-			return BitConverter.ToUInt32(_array, _pos++);
-		}
-		
-		public long ReadLong()
-		{
-			EnforceLength(sizeof(long));
-			//post addition returns the value then incriments it.
-			return BitConverter.ToInt64(_array, _pos++);
-		}
-		
-		public ulong ReadULong()
-		{
-			EnforceLength(sizeof(ulong));
-			//post addition returns the value then incriments it.
-			return BitConverter.ToUInt64(_array, _pos++);
-		}
-		
-		public float ReadSingle()
-		{
-			EnforceLength(sizeof(float));
-			//post addition returns the value then incriments it.
-			return BitConverter.ToSingle(_array, _pos++);
-		}
-		
-		public Double ReadDouble()
-		{
-			EnforceLength(sizeof(double));
-			//post addition returns the value then incriments it.
-			return BitConverter.ToDouble(_array, _pos++);
-		}
-		
-		public string ReadString()
-		{
-			ushort length = ReadUShort();
-			EnforceLength(length);
-			return _encoder.GetString(ReadBytes(length));
-		}
-	}
+    /// <summary>
+    /// Reads from a byte array.
+    /// </summary>
+    public class ByteReader
+    {
+        private byte[] _array;
+        private int _pos;
+        private ByteConverter _bc;
+        private Encoding _encoder;
+
+
+        public ByteReader(byte[] array)
+        {
+            _array = array;
+            init();
+
+        }
+        public ByteReader(Stream stream, int offset, int length)
+        {
+            stream.Read(_array, offset, length);
+            init();
+        }
+
+        public ByteReader(BinaryReader stream, int length)
+        {
+            _array = stream.ReadBytes(length);
+            init();
+        }
+
+        private void init()
+        {
+            _encoder = Encoding.Unicode;
+            _pos = 0;
+            _bc = new ByteConverter();
+        }
+
+
+        private ExecutionState EnforceLength(int length)
+        {
+            if (_pos + length >= _array.Length) return ExecutionState.Failed();
+            return ExecutionState.Succeeded();
+        }
+
+        public ExecutionState<byte> ReadByte()
+        {
+            if (!EnforceLength(sizeof(byte))) return ExecutionState<byte>.Failed();
+            return ExecutionState<byte>.Succeeded(_array[_pos++]);
+        }
+
+        public ExecutionState<byte[]> ReadBytes(int length)
+        {
+            if (!EnforceLength(sizeof(byte) * length)) return ExecutionState<byte[]>.Failed();
+            byte[] returnValue = new byte[length];
+            Array.Copy(_array, _pos, returnValue, 0, length);
+            _pos++;
+            return ExecutionState<byte[]>.Succeeded(returnValue);
+        }
+
+        public ExecutionState<sbyte> ReadSByte()
+        {
+            if (!EnforceLength(sizeof(sbyte))) return ExecutionState<sbyte>.Failed();
+            return ExecutionState<sbyte>.Succeeded(Convert.ToSByte(_array[_pos++]));
+        }
+
+        public ExecutionState<short> ReadShort()
+        {
+            if (!EnforceLength(sizeof(ushort))) return ExecutionState<short>.Failed();
+            var result = ExecutionState<short>.Succeeded(BitConverter.ToInt16(_array, _pos));
+            _pos += sizeof(ushort);
+            return result;
+        }
+
+        public ExecutionState<ushort> ReadUShort()
+        {
+            if (!EnforceLength(sizeof(ushort))) return ExecutionState<ushort>.Failed();
+            var result = ExecutionState<ushort>.Succeeded(BitConverter.ToUInt16(_array, _pos));
+            _pos += sizeof(ushort);
+            return result;
+        }
+
+        public ExecutionState<int> ReadInt()
+        {
+            if (!EnforceLength(sizeof(int))) return ExecutionState<int>.Failed();
+            var result = ExecutionState<int>.Succeeded(BitConverter.ToInt32(_array, _pos));
+            _pos += sizeof(int);
+            return result;
+        }
+
+        public ExecutionState<uint> ReadUInt()
+        {
+            if (!EnforceLength(sizeof(uint))) return ExecutionState<uint>.Failed();
+            var result = ExecutionState<uint>.Succeeded(BitConverter.ToUInt32(_array, _pos));
+            _pos += sizeof(uint);
+            return result;
+        }
+
+        public ExecutionState<long> ReadLong()
+        {
+            if (!EnforceLength(sizeof(long))) return ExecutionState<long>.Failed();
+            var result = ExecutionState<long>.Succeeded(BitConverter.ToInt64(_array, _pos));
+            _pos += sizeof(long);
+            return result;
+        }
+
+        public ExecutionState<ulong> ReadULong()
+        {
+            if (!EnforceLength(sizeof(ulong))) return ExecutionState<ulong>.Failed();
+            var result = ExecutionState<ulong>.Succeeded(BitConverter.ToUInt64(_array, _pos));
+            _pos += sizeof(ulong);
+            return result;
+        }
+
+        public ExecutionState<float> ReadSingle()
+        {
+            if (!EnforceLength(sizeof(float))) return ExecutionState<float>.Failed();
+            var result = ExecutionState<float>.Succeeded(BitConverter.ToSingle(_array, _pos));
+            _pos += sizeof(float);
+            return result;
+        }
+
+        public ExecutionState<double> ReadDouble()
+        {
+            if (!EnforceLength(sizeof(double))) return ExecutionState<double>.Failed();
+            var result = ExecutionState<double>.Succeeded(BitConverter.ToDouble(_array, _pos));
+            _pos += sizeof(double);
+            return result;
+        }
+
+        public ExecutionState<string> ReadString()
+        {
+            var stateLen = ReadUShort();
+            if (!stateLen) return ExecutionState<string>.Failed();
+            ushort length = stateLen.Result;
+            var stateData = ReadBytes(length);
+            if (!stateData) return ExecutionState<string>.Failed();
+            return ExecutionState<string>.Succeeded(_encoder.GetString(stateData.Result));
+        }
+    }
 }
